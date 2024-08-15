1e7fdce2073aa8622dd60e7a11dd8aa7c6d517be
Amayuru1999 <amarasingheau@gmail.com>
2024-08-16 00:52:15 +0530

    chore: Add CatService to handle cat-related operations

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatController } from './cat/cat.controller';
 import { CatsService } from './cats/cats.service';
+import { CatService } from './cat/cat.service';
 
 @Module({
   imports: [],
   controllers: [AppController, CatController],
-  providers: [AppService, CatsService],
+  providers: [AppService, CatsService, CatService],
 })
 export class AppModule {}
